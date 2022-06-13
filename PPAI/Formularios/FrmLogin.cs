b7b935fd6776660a9b7f493e85c332b6d94e0f0e
Sol Vega <sol.vega.01@gmail.com>
2022-06-12 23:04:30 -0300

    Form de Login

---
+++ PPAI/Formularios/FrmLogin.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PPAI.clases;
+
+namespace PPAI.Formularios
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text.Equals(""))
+            {
+                msgError("Por favor ingrese el nombre de usuario.");
+                txtUsuario.Focus();
+            }
+            else if (txtPassword.Text.Equals(""))
+            {
+                msgError("Por favor ingrese la contraseña.");
+                txtPassword.Focus();
+            }
+            else
+            {
+                string nombreDeUsuario = txtUsuario.Text.Trim();
+                string password = txtPassword.Text.Trim();
+                bool resultado = false;
+
+                //resultado = ValidarUsuario(nombreDeUsuario, password);
+                //if (resultado)
+                //{
+                //    Usuarios usu = new Usuarios(nombreDeUsuario, password);
+                //    FrmMenuPrincipal ventana = new FrmMenuPrincipal(usu);
+                //    ventana.Show();
+                //    ventana.FormClosed += LogOut;
+                //    Hide();
+                //}
+                //else
+                //{
+                //    msgError("Nombre de usuario o contraseña incorrectos, intente nuevamente.");
+                //    txtUsuario.Clear();
+                //    txtPassword.Clear();
+                //    txtUsuario.Focus();
+                //}
+            }
+        }
+
+        private void ValidarUsuario(string nombreDeUsuario, string password)
+        {
+            
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void msgError(string msg)
+        {
+            lblErrorMessage.Text = "      " +msg;
+            lblErrorMessage.Visible = true;
+        }
+        private void LogOut (object sender, FormClosedEventArgs e)
+        {
+            txtPassword.Clear();
+            txtUsuario.Clear();
+            lblErrorMessage.Visible=false;
+            this.Show();
+            txtUsuario.Focus();
+        }
+    }
+}
---
+++ PPAI/Formularios/FrmLogin.designer.cs
@@ -0,0 +1,182 @@
+﻿namespace PPAI.Formularios
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblIniciarSesion = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.imgUser = new System.Windows.Forms.PictureBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.lblErrorMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Font = new System.Drawing.Font("Open Sans Semibold", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(8, -9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(784, 190);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Gestión de Recursos Tecnológicos";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblIniciarSesion
+            // 
+            this.lblIniciarSesion.Font = new System.Drawing.Font("Open Sans Semibold", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIniciarSesion.Location = new System.Drawing.Point(8, 290);
+            this.lblIniciarSesion.Name = "lblIniciarSesion";
+            this.lblIniciarSesion.Size = new System.Drawing.Size(784, 72);
+            this.lblIniciarSesion.TabIndex = 1;
+            this.lblIniciarSesion.Text = "Iniciar Sesión";
+            this.lblIniciarSesion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(237)))), ((int)(((byte)(223)))));
+            this.txtUsuario.Location = new System.Drawing.Point(283, 372);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(283, 27);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // imgUser
+            // 
+            this.imgUser.Image = ((System.Drawing.Image)(resources.GetObject("imgUser.Image")));
+            this.imgUser.Location = new System.Drawing.Point(345, 184);
+            this.imgUser.Name = "imgUser";
+            this.imgUser.Size = new System.Drawing.Size(110, 105);
+            this.imgUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgUser.TabIndex = 3;
+            this.imgUser.TabStop = false;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(237)))), ((int)(((byte)(223)))));
+            this.txtPassword.Location = new System.Drawing.Point(283, 416);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(283, 27);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.Font = new System.Drawing.Font("Open Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(12, 372);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(265, 27);
+            this.lblUsuario.TabIndex = 5;
+            this.lblUsuario.Text = "Usuario:";
+            this.lblUsuario.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.Font = new System.Drawing.Font("Open Sans", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(12, 416);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(265, 27);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "Contraseña:";
+            this.lblPassword.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(117)))), ((int)(((byte)(255)))));
+            this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIngresar.Font = new System.Drawing.Font("Open Sans Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(36)))), ((int)(((byte)(35)))));
+            this.btnIngresar.Location = new System.Drawing.Point(304, 497);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(192, 45);
+            this.btnIngresar.TabIndex = 7;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = false;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // lblErrorMessage
+            // 
+            this.lblErrorMessage.AutoSize = true;
+            this.lblErrorMessage.BackColor = System.Drawing.Color.Transparent;
+            this.lblErrorMessage.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblErrorMessage.Image = ((System.Drawing.Image)(resources.GetObject("lblErrorMessage.Image")));
+            this.lblErrorMessage.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.lblErrorMessage.Location = new System.Drawing.Point(279, 463);
+            this.lblErrorMessage.Name = "lblErrorMessage";
+            this.lblErrorMessage.Size = new System.Drawing.Size(168, 22);
+            this.lblErrorMessage.TabIndex = 8;
+            this.lblErrorMessage.Text = "       Mensaje de Error";
+            this.lblErrorMessage.Visible = false;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(78)))));
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lblErrorMessage);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.imgUser);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblIniciarSesion);
+            this.Controls.Add(this.lblTitulo);
+            this.Font = new System.Drawing.Font("Bahnschrift SemiLight Condensed", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(219)))), ((int)(((byte)(213)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(12, 24, 12, 24);
+            this.MaximizeBox = false;
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestión de Recursos";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgUser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblIniciarSesion;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.PictureBox imgUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label lblErrorMessage;
+    }
+}
+
